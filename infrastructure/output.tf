afb5b600c5f79edf585a424aa6eca32866530b40
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2023-02-23 06:16:48 +0000

    fixing ansible issues

---
+++ infrastructure/output.tf
@@ -0,0 +1,13 @@
+output "ip" {
+  value = openstack_networking_floatingip_v2.floating-ip.address
+}
+
+resource "local_file" "ansible_inventory" {
+  filename        = "./inventory"
+  file_permission = "0644"
+  content         = templatefile("inventory.tpl", {
+    floating_ip = openstack_networking_floatingip_v2.floating-ip.address
+    username    = var.username
+    private_key = trimsuffix(var.key, ".pub")
+  })
+}
